847739bcbff08a19d202d1c6b06837e382da0686
NIKO <nzubovskiy@gmail.com>
2022-04-23 23:14:59 +0400

    Домашнее задание

---
+++ src/ru/skypro/Author.java
@@ -0,0 +1,19 @@
+package ru.skypro;
+
+public class Author {
+    String authorName;
+    String authorSurname;
+
+    public Author(String authorName, String authorSurname) {
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getAuthorSurname() {
+        return authorSurname;
+    }
+}
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,14 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Author author0 = new Author("Кэти", "Сьерра");
+        Author author1 = new Author("Джордж", "Оруэлл");
+
+        Book book0 = new Book("Изучаем Java", author0, 2012);
+        Book book1 = new Book("1984", author1, 2010);
+
+        book0.setYearOfPublication(2016);
+
     }
 }
